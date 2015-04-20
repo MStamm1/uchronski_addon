5829af3b0c1adf273b37a50ed4ea7b2addfe8bed
MStamm1 <Mstamm@re-gister.com>
2015-04-20 19:47:38 +0200

    Main files added

---
+++ data/uchronski.js
@@ -0,0 +1,13 @@
+
+var uploads = document.getElementsByClassName("s2d");
+//self.postMessage(uploads.length);
+
+//self.port.emit("pageEntriesLenght", uploads);
+
+/*self.port.on("replacePage", function(message) {
+  uploadsOld = message;
+  alert("uploadsOld = message - " + "uploads = " + uploads.length);
+});
+
+alert(uploadsOld);*/
+
